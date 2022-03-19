04efc650e3015ecf44c44ad87b7310f88e7f5a31
Avircut <lomoff.slaw@yandex.ru>
2022-03-19 19:19:35 +0700

    More adaptive fixes, burger created

--- app/js/main.js
+++ app/js/main.js
@@ -3,3 +3,23 @@ $('.slider__inner').slick({
   arrows:false,
   autoplay:false
 })
+
+$('.burger').on('click', () => {
+  $list = $('.nav__menu-item:not(.burger)');
+  if($('.burger__menu:has(*)').length){
+    $('.burger__menu-wrapper').toggleClass("burger__menu-wrapper_opened");
+    return;
+  }
+  $list.each((index,el)=>{
+    $clone = $(el).clone(true).removeClass('nav__menu-item').addClass('burger__menu-item');
+    $('.burger__menu').append($clone);
+  });
+  $('.burger__menu-item .nav__menu-dropdown').each((index, el) => {
+    $(el).removeClass('nav__menu-dropdown').addClass('burger__menu-dropdown');
+  })
+
+  $('.burger__menu-wrapper').toggleClass("burger__menu-wrapper_opened");
+})
+$('.burger__menu-close-btn').on('click', () => {
+  $('.burger').trigger('click');
+});
